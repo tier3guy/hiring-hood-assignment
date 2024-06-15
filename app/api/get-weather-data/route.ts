b371946db8520a14c8aa56d4f1d8a3af250d25a1
tier3guy <gavinash6290@gmail.com>
2024-06-15 19:36:33 +0530

    fix: return statement missing in route.ts in catch block

--- app/api/get-weather-data/route.ts
+++ app/api/get-weather-data/route.ts
@@ -46,7 +46,7 @@ export async function POST(request: NextRequest) {
     } catch (error) {
         console.log(error);
 
-        NextResponse.json(
+        return NextResponse.json(
             {
                 error: error,
                 data: null,
